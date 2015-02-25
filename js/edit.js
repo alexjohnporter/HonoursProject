0971df4a2122394cf10c81afb193e86f524d479a
Alex Porter <alexporter1992@live.co.uk>
2015-02-25 14:29:18 +0000

    slight refactor

--- js/edit.js
+++ js/edit.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //var urlRegex = /(http(s?):)|([/|.|\w|\s])*\.(?:jpg|gif|png)/igm;
-    function testUrl(url){
+    function testUrl(url) {
         var regex = new RegExp(
             "^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
         return regex.test(url);
     }
-    var headerUpdate = $.getJSON('php/select/jsonHeader.php', function(data){
-        $.each(data, function (i,ent){
+    var headerUpdate = $.getJSON('php/select/jsonHeader.php', function (data) {
+        $.each(data, function (i, ent) {
             $('#hdr-txt-update').attr('value', ent['hdrTxt']);
             $('#author-update').attr('value', ent['author']);
             $('#fb-update').attr('value', ent['fbIcon']);
             $('#twit-update').attr('value', ent['twitIcon']);
             $('#header-id').attr('value', ent['hdrID']);
         });
-        headerUpdate.done(function(){
-            $('#header-form').submit(function(){
+        headerUpdate.done(function () {
+            $('#header-form').submit(function () {
                 var fb = $('#fb-update').val();
                 var twit = $('#twit-update').val();
-                if(!testUrl(fb,twit) || $('#header-form :input').val() == ""){
+                if (!testUrl(fb, twit) || $('#header-form :input').val() === "") {
                     $('.update-response').html('Please enter valid information.');
                     $('#header-update-error').show('fast').fadeOut(2000);
-                }else {
+                } else {
                     var formData = $(this).serialize();
                     console.log(formData);
                     var updateHeader = $.post('php/update/updateHeader.php', formData, function (data) {
@@ -42,10 +42,9 @@ $(document).ready(function(){
         });
     });
 
-
-    $('#time-select-button').click(function(){
-        var timeId = $(this).parents('form').find('select').val()
-        var showTime = $.getJSON("php/select/selectTime.php?q="+timeId, function(data) {
+    $('#time-select-button').click(function () {
+        var timeId = $(this).parents('form').find('select').val();
+        var showTime = $.getJSON("php/select/selectTime.php?q=" + timeId, function (data) {
             $.each(data, function (i, ent) {
                 $('#mon').attr('value', ent['Mon']);
                 $('#mon-close').attr('value', ent['MonClose']);
@@ -63,12 +62,12 @@ $(document).ready(function(){
                 $('#sun-close').attr('value', ent['SunClose']);
                 $('#time-id-update').attr('value', ent['timeID']);
             });
-            showTime.done(function(){
+            showTime.done(function () {
                 $('#time-update-form').submit(function () {
-                    if($('#time-update-form :input').val() == ""){
+                    if ($('#time-update-form :input').val() === "") {
                         $('.update-response').html('Please enter valid information.');
                         $('#time-update-error').show('fast').fadeOut(2000);
-                    }else {
+                    } else {
                         var formData = $(this).serialize();
                         console.log(formData);
                         var updateTime = $.post('php/update/updateTiming.php', formData, function (data) {
@@ -89,76 +88,71 @@ $(document).ready(function(){
         });
     });
 
-    var  itemJson = $.getJSON("php/select/json.php", function(data) {
-        $.each(data, function(i,ent){
-           $("<div class='item-edit-container col-lg-4'>"+
-            "<h2>" + ent['itemName'] + "</h2>"+
-            "<p><b>Description:</b> " + ent['itemTxt'] + "</p>"+
-            "<p><b>Address line one:</b> " + ent['AddressOne'] + "</p>"+
-            "<p><b>Address line two:</b> " + ent['AddressTwo'] + "</p>"+
-            "<p><b>Postcode:</b> " + ent['Postcode'] + "</p>"+
-            "<p><b>Image URL:</b> <a href='" + ent['itemImg'] + "'>" + ent['itemImg'] + "</a></p>"+
-            "<p><b>Time ID:</b> " + ent['timeID'] + "</p>"+
-            "<p class='item-id'>"+ent['itemID']+"</p>"+
-            '<div class="btn-group" role="group" aria-label="crud-btn">'+
-            "<button type='button' class='btn btn-default btn-primary btn-lg update' data-toggle='modal' data-target='#item-modal'>Update</button>"+
+    var itemJson = $.getJSON("php/select/json.php", function (data) {
+        $.each(data, function (i, ent) {
+            $("<div class='item-edit-container col-lg-4'>" +
+            "<h2>" + ent['itemName'] + "</h2>" +
+            "<p><b>Description:</b> " + ent['itemTxt'] + "</p>" +
+            "<p><b>Address line one:</b> " + ent['AddressOne'] + "</p>" +
+            "<p><b>Address line two:</b> " + ent['AddressTwo'] + "</p>" +
+            "<p><b>Postcode:</b> " + ent['Postcode'] + "</p>" +
+            "<p><b>Image URL:</b> <a href='" + ent['itemImg'] + "'>" + ent['itemImg'] + "</a></p>" +
+            "<p><b>Time ID:</b> " + ent['timeID'] + "</p>" +
+            "<p class='item-id'>" + ent['itemID'] + "</p>" +
+            '<div class="btn-group" role="group" aria-label="crud-btn">' +
+            "<button type='button' class='btn btn-default btn-primary btn-lg update' data-toggle='modal' data-target='#item-modal'>Update</button>" +
             "<button type='button' class='btn btn-default btn-danger btn-lg delete'  data-toggle='modal' data-target='#item-modal-delete'>Delete</button></div>").appendTo('#edit-page');
         });
 
-            itemJson.done(function () {
-                var itemID;
-                $('.update').click(function(){
-                    var itemID = parseInt($(this).parents('.item-edit-container').find('.item-id').text());
-                    console.log(itemID);
-
-                    $.getJSON("php/select/json.php?q="+itemID, function(data) {
-                        $.each(data, function(i,ent) {
-                            $('#item-name-edit').attr('value', ent['itemName']);
-                            $('#first-line-edit').attr('value', ent['AddressOne']);
-                            $('#second-line-edit').attr('value', ent['AddressTwo']);
-                            $('#postcode-edit').attr('value', ent['Postcode']);
-                            $('#item-img-edit').attr('value', ent['itemImg']);
-                            $('#item-txt-edit').text(ent['itemTxt']);
-                            $('#item-id-edit').attr('value', ent['itemID']);
-                            $('#delete-item').attr('value', ent['itemID']);
-                        });
-                     });
-
-                });
-
-                $('.delete').click(function(){
-                    var itemID = parseInt($(this).parents('.item-edit-container').find('.item-id').text());
-                    console.log(itemID);
-
-                    $.getJSON("php/select/json.php?q="+itemID, function(data) {
-                        $.each(data, function(i,ent) {
-                            $('#delete-item').attr('value', ent['itemID']);
-                        });
+        itemJson.done(function () {
+            var itemID;
+            $('.update').click(function () {
+                var itemID = parseInt($(this).parents('.item-edit-container').find('.item-id').text());
+                console.log(itemID);
+                $.getJSON("php/select/json.php?q=" + itemID, function (data) {
+                    $.each(data, function (i, ent) {
+                        $('#item-name-edit').attr('value', ent['itemName']);
+                        $('#first-line-edit').attr('value', ent['AddressOne']);
+                        $('#second-line-edit').attr('value', ent['AddressTwo']);
+                        $('#postcode-edit').attr('value', ent['Postcode']);
+                        $('#item-img-edit').attr('value', ent['itemImg']);
+                        $('#item-txt-edit').text(ent['itemTxt']);
+                        $('#item-id-edit').attr('value', ent['itemID']);
+                        $('#delete-item').attr('value', ent['itemID']);
                     });
-
                 });
             });
 
-             itemJson.fail(function(){
-                console.log('Header JSON has failed to parse');
-                });
-             });
+            $('.delete').click(function () {
+                var itemID = parseInt($(this).parents('.item-edit-container').find('.item-id').text());
+                console.log(itemID);
 
-    $.getJSON("php/select/selectTime.php", function(data) {
-        $.each(data, function(i,ent) {
-            $('<option value="'+ ent['timeID']+'">'+ent['timeID'] +'</option>').appendTo('.edit-time-id');
-            $('<option value="'+ ent['timeID']+'">'+ent['timeID'] +'</option>').appendTo('.insert-time-id');
+                $.getJSON("php/select/json.php?q=" + itemID, function (data) {
+                    $.each(data, function (i, ent) {
+                        $('#delete-item').attr('value', ent['itemID']);
+                    });
+                });
+            });
+        });
+
+        itemJson.fail(function () {
+            console.log('Header JSON has failed to parse');
         });
     });
 
+    $.getJSON("php/select/selectTime.php", function (data) {
+        $.each(data, function (i, ent) {
+            $('<option value="' + ent['timeID'] + '">' + ent['timeID'] + '</option>').appendTo('.edit-time-id');
+            $('<option value="' + ent['timeID'] + '">' + ent['timeID'] + '</option>').appendTo('.insert-time-id');
+        });
+    });
 
-
-    $('#edit-item-form').submit(function(){
+    $('#edit-item-form').submit(function () {
         var url = $('#item-img-edit').val();
-        if($('#edit-item-form :input').val() == "" || !testUrl(url) || $('#item-txt-edit').val()=="" || $('#item-txt-edit').val() > 500){
+        if ($('#edit-item-form :input').val() == "" || !testUrl(url) || $('#item-txt-edit').val() == "" || $('#item-txt-edit').val() > 500) {
             $('.update-response').html('Please enter valid information.');
             $('#update-error').show('fast').fadeOut(2000);
-        }else {
+        } else {
             var formData = $(this).serialize();
             var updateItem = $.post('php/update/update.php', formData, function (data) {
 
@@ -172,26 +166,20 @@ $(document).ready(function(){
                     $('.update-response').html('Update failed.');
                     $('#update-error').show('fast').fadeOut(2000);
                 }
-
-
             });
-
             updateItem.fail(function (status, err) {
-                console.log('form submission failed');
-
+                console.log(status + ", " +err +' form submission failed');
             });
-
-
             return false; //make sure page doesn't refresh on form submit
         }
     });
 
-    $('#insert-item-form').submit(function(){
+    $('#insert-item-form').submit(function () {
         var url = $('#item-img-insert').val();
-        if($('#insert-item-form :input').val() == "" || !testUrl(url)|| $('#item-txt-insert').val()=="" || $('#item-txt-insert').val() > 500){
+        if ($('#insert-item-form :input').val() == "" || !testUrl(url) || $('#item-txt-insert').val() == "" || $('#item-txt-insert').val() > 500) {
             $('.insert-response').html('Please enter valid information.');
             $('#insert-error').show('fast').fadeOut(2000);
-        }else {
+        } else {
             var formData = $(this).serialize();
             console.log(formData);
             var insertItem = $.post('php/insert/insert.php', formData, function (data) {
@@ -206,47 +194,31 @@ $(document).ready(function(){
                     $('.insert-response').html('Insert failed.');
                     $('#insert-error').show('fast').fadeOut(2000);
                 }
-
-
             });
-
             insertItem.fail(function (status, err) {
                 console.log('form submission failed');
-
             });
-
-
             return false; //make sure page doesn't refresh on form submit
         }
     });
 
-    $('#delete-item-form').submit(function(){
+    $('#delete-item-form').submit(function () {
+        var formData = $(this).serialize();
+        console.log(formData);
+        var insertItem = $.post('php/delete/delete.php', formData, function (data) {
 
-            var formData = $(this).serialize();
-             console.log(formData);
-          var insertItem = $.post('php/delete/delete.php', formData, function (data) {
+            if (data.status !== true) {
+                window.location.reload(true);
 
-                if (data.status !== true) {
+            } else {
+                alert('Item deletion failed');
+            }
+        });
 
-                      window.location.reload(true);
+        insertItem.fail(function (status, err) {
+            console.log('form submission failed');
 
-
-                } else {
-                 alert('Item deletion failed');
-                }
-
-
-            });
-
-            insertItem.fail(function (status, err) {
-                console.log('form submission failed');
-
-            });
-
-
-            return false; //make sure page doesn't refresh on form submit
-
+        });
+        return false; //make sure page doesn't refresh on form submit
     });
 });
-
-
--- js/maps.js
+++ js/maps.js
@@ -1,12 +1,10 @@
-
-var geocoder, map, myLat, myLong, currLat, currLong,currPos, directionsDisplay, marker;
+var geocoder, map, myLat, myLong, currLat, currLong, currPos, directionsDisplay, marker;
 var directionsService = new google.maps.DirectionsService();
 
 var mapOptions = {
     zoom: 17,
     mapTypeId: google.maps.MapTypeId.ROADMAP
 };
-
 
 function init(address) {
     geocoder = new google.maps.Geocoder();
@@ -16,24 +14,22 @@ function init(address) {
     getCoords(address);
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(document.getElementById('directions-panel'));
-    setTimeout(function(){
+    setTimeout(function () {
         var center = map.getCenter();
         google.maps.event.trigger(map, "resize");
         map.setCenter(center);
     }, 500);
-
-
 }
 
 
-google.maps.event.addDomListener(window, "resize", function() {
-    if(map){
-    var center = map.getCenter();
-    google.maps.event.trigger(map, "resize");
-    map.setCenter(center);
-    console.log('map resized');
-    return center;
-    } else{
+google.maps.event.addDomListener(window, "resize", function () {
+    if (map) {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+        console.log('map resized');
+        return center;
+    } else {
         console.log('map not loaded');
     }
 });
@@ -41,17 +37,18 @@ google.maps.event.addDomListener(window, "resize", function() {
 
 function getCoords(address) {
 
-    geocoder.geocode( { 'address': address}, function(results, status) {
+    geocoder.geocode({
+        'address': address
+    }, function (results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             map.setCenter(results[0].geometry.location);
             google.maps.event.trigger(window.map, 'resize');
-            if(marker)
-                marker.setMap(null);
+            if (marker) marker.setMap(null);
             marker = new google.maps.Marker({
                 map: map,
                 position: results[0].geometry.location
 
-        });
+            });
 
         } else {
             console.log('failure: ' + status);
@@ -60,51 +57,46 @@ function getCoords(address) {
     });
 }
 
-var geoStatus  = true;
+var geoStatus = true;
 
-if(navigator.geolocation){
+if (navigator.geolocation) {
     console.log('geolocation is on!');
 
-    window.onload = function(){
-        navigator.geolocation.getCurrentPosition(function(pos){
-          myLat = pos.coords.latitude;
-          myLong = pos.coords.longitude; //start  lat and long
-            return parseFloat(myLat) + ',' + parseFloat(myLong);
-        },
+    window.onload = function () {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+                myLat = pos.coords.latitude;
+                myLong = pos.coords.longitude; //start  lat and long
+                return parseFloat(myLat) + ',' + parseFloat(myLong);
+            },
 
-        function(err){
-          console.log(err+": Geolocation is disabled. Please turn it on and try again.");
-            geoStatus = false;
+            function (err) {
+                console.log(err + ": Geolocation is disabled. Please turn it on and try again.");
+                geoStatus = false;
 
-        });
+            });
         navigator.geolocation.watchPosition(function (pos) {
-           currLat = pos.coords.latitude;
-           currLong = pos.coords.longitude; //current lat and long
+            currLat = pos.coords.latitude;
+            currLong = pos.coords.longitude; //current lat and long
             currPos = parseFloat(currLat) + ',' + parseFloat(currLong);
         });
-    }
+    };
 
-} else{
+} else {
     console.log("geolocation isn't available");
 }
 
+function calcRoute(startLoc, endLoc, transportMethod) { //standard google-maps directions function adapted to my project
+    var request = {
+        origin: startLoc,
+        destination: endLoc,
+        travelMode: transportMethod
+    };
+    directionsService.route(request, function (response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            console.log(status);
+        }
+    });
 
-
-function calcRoute(startLoc,endLoc,transportMethod) { //standard google-maps directions function adapted to my project
-        var request = {
-            origin:startLoc,
-            destination:endLoc,
-            travelMode: transportMethod
-        };
-        directionsService.route(request, function(response, status) {
-            if (status == google.maps.DirectionsStatus.OK) {
-                directionsDisplay.setDirections(response);
-            }
-            else{
-                console.log(status);
-            }
-        });
-//currLat + ',' + currLong;
 }
-
-
